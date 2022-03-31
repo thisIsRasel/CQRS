d107582d7147775e4586e1726669e7c481dc0b53
ThisIsRasel <raselcse20@gmail.com>
2022-04-01 01:57:25 +0600

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Application.CreateStudent;
+using Application.CreateTeacher;
+using Application.GetStudents;
+using Domain;
+using Domain.Aggregates.StudentAggregate;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public StudentController(
+            ICommandDispatcher commandDispatcher,
+            IQueryDispatcher queryDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllStudents([FromQuery]GetStudentsQuery query)
+        {
+            var result = await _queryDispatcher
+                .DispatchAsync<GetStudentsQuery, IReadOnlyList<Student>>(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateStudent([FromBody] CreateStudentCommand command)
+        {
+            var response = await _commandDispatcher
+                .DispatchAsync<CreateStudentCommand, bool>(command);
+
+            return Ok(response);
+        }
+    }
+}
