abeb9fa1dce0a4c17df5bc45bbcac16889cfdee9
Rasel <rasel.ahammed@selise.ch>
2022-04-14 03:11:36 +0600

    Command Dispatcher Test Added

---
+++ src/UnitTests/Application/CreateStudentCommandHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Application.CreateStudent;
+using Domain.Aggregates.StudentAggregate;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace UnitTests.Application
+{
+    public class CreateStudentCommandHandlerTests
+    {
+        [Fact(DisplayName = "Should Create Student")]
+        public async Task ShouldCreateStudent()
+        {
+            var command = new CreateStudentCommand
+            {
+                FirstName = "Rasel",
+                LastName = "Ahammed",
+            };
+
+            var mocker = new AutoMocker();
+            var handler = mocker.CreateInstance<CreateStudentCommandHandler>();
+
+            var studentWriteRepository = mocker.GetMock<IStudentWriteRepository>();
+            studentWriteRepository
+                .Setup(r => r.CreateStudentAsync(It.IsAny<Student>()))
+                .Returns(Task.CompletedTask);
+
+            var response = await handler.HandleAsync(command);
+            Assert.True(response);
+        }
+    }
+}
---
+++ src/UnitTests/Infrastructure/CommandDispatcherTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain;
+using Infrastructure;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace UnitTests.Infrastructure
+{
+    public class CommandDispatcherTests
+    {
+        [Fact(DisplayName = "Should throw exception if command handler not found")]
+        public void ShouldThrowExceptionIfCommandHandlerNotFound()
+        {
+            var command = new TestCommand();
+
+            var mocker = new AutoMocker();
+            var commandDispatcher = mocker.CreateInstance<CommandDispatcher>();
+
+            Task Func() => commandDispatcher
+                .DispatchAsync<TestCommand, bool>(command);
+
+            Assert.ThrowsAsync<InvalidOperationException>(Func);
+        }
+
+        [Fact(DisplayName = "Should dispatch if command handler found")]
+        public async Task ShouldDispatchIfCommandHandlerFound()
+        {
+            var command = new TestCommand();
+            var commandHandler = new TestCommandHandler();
+
+            var mocker = new AutoMocker();
+            var commandDispatcher = mocker.CreateInstance<CommandDispatcher>();
+
+            var serviceProvider = mocker.GetMock<IServiceProvider>();
+            serviceProvider
+                .Setup(x => x.GetService(It.IsAny<Type>()))
+                .Returns(commandHandler);
+
+            var response = await commandDispatcher
+                .DispatchAsync<TestCommand, bool>(command);
+
+            Assert.True(response);
+        }
+
+        public class TestCommand
+        {
+
+        }
+
+        public class TestCommandHandler : ICommandHandler<TestCommand, bool>
+        {
+            public Task<bool> HandleAsync(TestCommand command)
+            {
+                return Task.FromResult(true);
+            }
+        }
+    }
+}
